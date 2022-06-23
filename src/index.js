87a3b240d38865d735fa8a055b933a8491e55a4f
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-06-23 12:40:20 +0500

    init: structure of project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// scss
+import 'normalize.css/normalize.css';
+import './scss/base.scss';
+
+// js
+
+const favicon = document.createElement('link');
+favicon.rel = 'shortcut icon';
+favicon.href = 'https://rs.school/favicon.ico';
+document.head.appendChild(favicon);
